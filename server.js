93399611cb8220f991fa8d5bee5b11a79be26b0c
temptationkentos57 <kreetlannoo@hotmail.com>
2025-10-04 18:52:02 +0200

    feat: initial commit for server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+// Kết nối đến MongoDB
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Kết nối đến MongoDB thành công'))
+  .catch(err => console.log(err));
+
+app.listen(PORT, () => {
+  console.log(`Server đang chạy trên cổng ${PORT}`);
+});
